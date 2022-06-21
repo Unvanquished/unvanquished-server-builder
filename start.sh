321d8bddd61216941e415f408bcab9146f339885
Antoine Fontaine <antoine.fontaine@epfl.ch>
2022-06-21 08:25:50 +0200

    Print status from the updater

--- start.sh
+++ start.sh
@@ -15,6 +15,10 @@ exec tmux -L testing-server new-session \
 		if [ $? -ne 0 ]; then
 			printf "\nDeployment FAILED\n"
 		fi
+
+		sleep 5
+		~/unv-testing-server/update.sh status
+		.
 		printf "\n"
 		for time in $(seq $((60*3)) -1 1); do
 			printf "\rRunning update script again in %i seconds  " $time
