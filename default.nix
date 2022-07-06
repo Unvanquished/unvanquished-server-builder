c5d647440714d8652322c77b44eed67c24b8e82d
Antoine Fontaine <antoine.fontaine@epfl.ch>
2022-07-06 12:26:32 +0200

    Use unvanquished.git /dist/configs to import the config

--- default.nix
+++ default.nix
@@ -98,11 +98,6 @@ in rec {
         GDB="${gdb}/bin/gdb -x $HOME/unv-testing-server/gdbinit.txt --args"
     fi
 
-    # set a default value for $@ if there is no arguments to this script
-    if [ $# -eq 0 ]; then
-        set -- +devmap chasm +bot fill 4
-    fi
-
     exec tmux -L testing-server new-session -s ${tmux-session-name} -d \
         ${bubblewrap}/bin/bwrap \
             --unshare-all --share-net \
@@ -111,6 +106,8 @@ in rec {
             --ro-bind /var/www/cdn.unvanquished.net/unvanquished_0.52.1/pkg /var/www/cdn.unvanquished.net/unvanquished_0.52.1/pkg \
             --bind ${homepath} ${homepath} \
             --ro-bind ~/unvanquished-server/homepath/game/admin.dat ${homepath}/game/admin.dat \
+            --ro-bind ${srcs.unvanquished}/dist/configs/config ${homepath}/config \
+            --ro-bind ${srcs.unvanquished}/dist/configs/game/maprotation.cfg ${homepath}/game/maprotation.cfg \
             --ro-bind ~/unv-testing-server/gdbinit.txt ~/unv-testing-server/gdbinit.txt \
             --tmpfs /tmp \
             --proc /proc \
@@ -124,18 +121,19 @@ in rec {
                         -libpath ${unvanquished-vms} \
                         -homepath ${homepath} \
                         -set vm.sgame.type 3 \
-                        -set server.private 1 \
-                        -set sv_hostname "^1Experimental ^3Development Server - ${servername}" \
-                        -set g_motd "^2See the ${branchname} branch on GitHub." \
-                        -set sv_allowdownload 1 \
-                        -set sv_dl_maxRate 1000000 \
-                        -set sv_wwwDownload 1 \
-                        -set sv_wwwBaseURL "users.unvanquished.net/~afontain/pkg/nightly" \
-                        -set sv_wwwFallbackURL "dl.unvanquished.net/pkg" \
                         -set net_port 27990 \
                         -pakpath ${pakpath} \
                         -pakpath /var/www/cdn.unvanquished.net/unvanquished_0.52.1/pkg \
                         -set fs_extrapaks experimental/${servername}/${filename} \
+                        +exec server.cfg \
+                        +set server.private 1 \
+                        +set sv_hostname "^1Experimental ^3Development Server - ${servername}" \
+                        +set g_motd "^2See the ${branchname} branch on GitHub." \
+                        +set sv_allowdownload 1 \
+                        +set sv_dl_maxRate 1000000 \
+                        +set sv_wwwDownload 1 \
+                        +set sv_wwwBaseURL "users.unvanquished.net/~afontain/pkg/nightly" \
+                        +set sv_wwwFallbackURL "dl.unvanquished.net/pkg" \
                         "$@"
     '';
 }
