92763ebbbb99336722334ef173262357014f9dfa
Antoine Fontaine <antoine.fontaine@epfl.ch>
2023-02-03 18:14:20 +0100

    Move updater to a separate script

--- start-updater.sh
+++ start-updater.sh
@@ -6,29 +6,5 @@ fi
 
 exec tmux -L testing-server new-session \
        	-s automated-upgrade \
-       	-c ~/unv-testing-server \
-	'
-	last_status=0
-	while true; do
-		~/unv-testing-server/update.sh fetch && \
-		~/unv-testing-server/update.sh compile && \
-		~/unv-testing-server/update.sh deploy
-
-		if [ $? -ne 0 ]; then
-			printf "\nDeployment FAILED\n"
-			[ $last_status = 0 ] && sudo -u overmind /home/overmind/msg "#unvanquished-dev" "Testing servers update \x0308failed"
-			last_status=1
-		else
-			last_status=0
-		fi
-
-		sleep 5
-		~/unv-testing-server/update.sh status
-
-		printf "\n"
-		for time in $(seq $((60*3)) -1 1); do
-			printf "\rRunning update script again in %i seconds  " $time
-			sleep 1
-		done
-		printf "\n\n"
-	done'
+		-c ~/unv-testing-server \
+	~/unv-testing-server/updater.sh
